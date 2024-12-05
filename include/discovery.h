69bae5fecada036598662e8c5fa832a1257151c6
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2024-12-05 16:42:22 +0000

    Reading the sensor made more robust. Still not perfect.

--- include/discovery.h
+++ include/discovery.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "mdns.h"
-
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 bool discovery_start();
 bool discovery_init(const char *json, bool *save);
 mdns_result_t *discovery_find_mdns_service(const char *type, const char *proto);
+#ifdef __cplusplus
+}
+#endif
